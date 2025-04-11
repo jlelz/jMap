f96d02677e6bb4b0130a78a42fca3d2b7c8d8dea
jlelz <79492325+jlelz@users.noreply.github.com>
2025-04-10 23:06:12 -0600

    scaling

--- App.lua
+++ App.lua
@@ -48,6 +48,16 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
                 if( InCombatLockdown() ) then
                     return;
                 end
+                --[[
+                local InInstance,InstanceType = IsInInstance();
+                if( InInstance ) then
+                    if( Addon.APP:GetValue( 'CloseWhileInstanced' ) ) then
+                        if( WorldMapFrame:IsShown() ) then
+                            WorldMapFrame:Hide();
+                        end
+                    end
+                end
+                ]]
                 if( Event == 'PLAYER_STARTED_MOVING' or Event == 'PLAYER_STARTED_LOOKING' or Event == 'PLAYER_STARTED_TURNING' ) then
                     WorldMapFrame:SetAlpha( Addon.APP:GetValue( 'MapAlpha' ) );
                     Addon.APP:Ping();
@@ -116,7 +126,7 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
                 Step = .1,
                 KeyPairs = {
                     Low = {
-                        Value = .5,
+                        Value = .3,
                         Description = 'Low',
                     },
                     High = {
